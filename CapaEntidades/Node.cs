178963d25790a57de7b2d02650374fdb3eff487c
Nitid Developing <nitiddeveloping@gmail.com>
2023-10-17 00:13:42 -0300

    """"""Implementación del mapa""""""

---
+++ CapaEntidades/Node.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaEntidades
+{
+    public class Node
+    {
+        public int X { get; }
+        public int Y { get; }
+        public bool IsWall { get; set; }
+        public int GCost { get; set; }
+        public int HCost { get; set; }
+        public int FCost => GCost + HCost;
+        public Node Parent { get; set; }
+        public bool IsPath { get; set; }
+
+        public Node(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public void Reset()
+        {
+            GCost = 0;
+            HCost = 0;
+            Parent = null;
+            IsPath = false;
+        }
+    }
+}
